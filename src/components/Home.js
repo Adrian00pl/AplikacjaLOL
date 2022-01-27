b6b47e98086fdeef1e1a64b015db7fdadeafec9c
CrackRockefeller <adamwwujkiewicz@gmail.com>
2022-01-27 15:02:04 +0100

    UPDATE

--- src/components/Home.js
+++ src/components/Home.js
@@ -14,8 +14,8 @@ const Home = () => {
     <div className='body'>
         
         <div class="Slider">
-        <p className='tytul'>League of Legens Players Stats</p>
-        <AliceCarousel autoPlay autoPlayInterval="2500" infinite>
+        <p className='tytul'>League of Legends Players Stats</p>
+        <AliceCarousel autoPlay autoPlayInterval="2500" infinite disableButtonsControls>
         <img src={IMG1} className="sliderimg" className='bobo'/>
         <img src={IMG2} className="sliderimg" className='bobo'/>
         <img src={IMG3} className="sliderimg" className='bobo'/>
